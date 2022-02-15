a671e3156c27b186afed8a6288ce5757909b1743
NicoEvgl <nico.evangelista33@gmail.com>
2022-02-15 07:36:08 +0100

    add sidebar

--- escore-webclient/src/components/header/TeamHeader.js
+++ escore-webclient/src/components/header/TeamHeader.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
+import { RiMenuLine, RiMoonLine } from "react-icons/ri";
+import { CgLogIn } from "react-icons/cg";
+import { BiHome } from "react-icons/bi";
 
 const TeamHeader = () => {
+  const sidebarCollapsed = localStorage.getItem("sidebar-collapsed");
+  const [isExpanded, setIsExpanded] = useState(sidebarCollapsed ? false : true);
+
+  const handleToggler = () => {
+    if (isExpanded) {
+      setIsExpanded(false);
+      localStorage.setItem("sidebar-collapsed", true);
+      return;
+    }
+    setIsExpanded(true);
+    localStorage.removeItem("sidebar-collapsed");
+  };
+
   return (
     <div className="team-header">
       <NavLink to={"/"}>
@@ -11,6 +27,26 @@ const TeamHeader = () => {
           </span>
         </div>
       </NavLink>
+      <div className={isExpanded ? "sidebar" : "sidebar collapsed"}>
+        <div className="sidebar-btn">
+          <RiMenuLine className="sidebar-menu-icon" onClick={handleToggler} />
+          <span className="hide-visually">Menu</span>
+        </div>
+        <div className="sidebar-items">
+          <div className="item">
+            <BiHome className="sidebar-icon" />
+            <span className="sidebar-text">Accueil</span>
+          </div>
+          <div className="item">
+            <CgLogIn className="sidebar-icon" />
+            <span className="sidebar-text">Connexion</span>
+          </div>
+          <div className="item">
+            <RiMoonLine className="sidebar-icon" />
+            <span className="sidebar-text">Dark Mode</span>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
