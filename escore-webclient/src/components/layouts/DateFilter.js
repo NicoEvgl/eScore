fddf1832be28eed950dc8b59b8de87a53d9fd496
NicoEvgl <nico.evangelista33@gmail.com>
2022-03-12 22:59:37 +0100

    import DatePicker & edit DateFilter

--- escore-webclient/src/components/layouts/DateFilter.js
+++ escore-webclient/src/components/layouts/DateFilter.js
@@ -1,25 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import { BsFillCaretLeftFill, BsFillCaretRightFill } from "react-icons/bs";
+import DatePicker from "react-datepicker";
+
+// import required react-datepicker styling file
+import "react-datepicker/dist/react-datepicker.css";
 
 const DateFilter = () => {
+  const [startDate, setStartDate] = useState(new Date());
   return (
     <div className="date-filter">
+      {/*
       <NavLink to={"/"} className="date-filter-nav">
         <BsFillCaretLeftFill className="date-filter-icon"></BsFillCaretLeftFill>
         <span className="hide-visually">Previous Day</span>
       </NavLink>
-      <select onchange="" name="" id="" class="date-filter-selector">
-        <option value="#">Mercredi 02 Février 2022</option>
-        <option value="#" selected="">
-          Jeudi 03 Février 2022
-        </option>
-        <option value="#">Vendredi 04 Février 2022</option>
-      </select>
+      */}
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+        dateFormat="yyyy/MM/dd"
+        className="date-filter-selector"
+      />
+      {/*
       <NavLink to={"/"} className="date-filter-nav">
         <BsFillCaretRightFill className="date-filter-icon"></BsFillCaretRightFill>
         <span className="hide-visually">Next Day</span>
       </NavLink>
+    */}
     </div>
   );
 };
